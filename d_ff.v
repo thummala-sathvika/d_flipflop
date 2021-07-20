6ea33ed8ea54967a939f425f5dd4f1e383bc63b1
Thummala Sathvika <68094844+thummala-sathvika@users.noreply.github.com>
2021-07-20 21:55:55 +0530

    Create d_ff.v

---
+++ d_ff.v
@@ -0,0 +1,30 @@
+
+ 
+module d_ff(clk,
+           reset,
+           d,
+           q,
+           qb);
+
+// Step1 : Declare Port Directions
+
+  input clk,reset,d;	 
+  output reg q;
+  output qb;
+
+// Step2 : Write the behavioral logic for D flip-flop functionality.
+
+  always@(posedge clk)
+  begin
+    if(reset)
+      q=0;
+    else
+      q=d;
+  end
+
+//Step3. Assign complement of q to qb.
+
+  assign qb=~q;       
+
+endmodule         
+
